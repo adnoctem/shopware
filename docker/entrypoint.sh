ac6c45b92e929e201076523f6a01dbc0fa2e1a55
Maximilian Gindorfer <maximilian.gindorfer@delta4x4.com>
2024-07-03 14:04:01 +0200

    com: prepare Docker images

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env sh
+
+# Get the PUID and PGID from environment variables (or use default values 1000 if not set)
+export PUID="${PUID:-1001}"
+export PGID="${PGID:-1001}"
+
+chown -R "${PUID}:${PGID}" .
+
+fmjctl run
